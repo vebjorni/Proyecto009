4eadc472d67856f4b00543ce88d94cc9a93a10c6
vebjorni <raulsanchezgonzalez1@gmail.com>
2019-11-20 10:13:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/proyecto009/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.proyecto009;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1,et2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.et1);
+        et2=(EditText)findViewById(R.id.et2);
+    }
+    public void verificar(View v) {
+        String clave = et2.getText().toString();
+        if (clave.length() == 0) {
+            Toast notificacion = Toast.makeText(this, "La clave no puede quedar vacía", Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+    }
+}
